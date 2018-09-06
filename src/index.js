d9f6c8ccc152bcf6a2e713f0a1db7550d560ac1f
Mateus Paulino <mateuspaulino@MacBook-Pro-de-Mateus.local>
2018-09-06 03:12:27 -0300

    comment structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './../static/sass/main.scss';
 
 async function init() {
   const $ = document.querySelector.bind(document);
-  $('.carousel-container').innerHTML = Carousel();
+  // $('.carousel-container').innerHTML = Carousel();
 }
 
 init();
