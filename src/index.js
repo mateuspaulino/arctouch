1fac46ae41291472a1816a531864c0f3f3d4b607
Mateus Paulino <contato@mateuspaulino.com.br>
2018-09-06 13:23:20 -0300

    carousel structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import Carousel from './components/Carousel';
+// import Carousel from './components/Carousel';
 import './../static/sass/main.scss';
 
 async function init() {
   const $ = document.querySelector.bind(document);
+  const $$ = document.querySelectorAll.bind(document);
+
+  
+
   // $('.carousel-container').innerHTML = Carousel();
 }
 
