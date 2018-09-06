772cd279de251ccd0ef5c2406ede61761ff33d2b
Mateus Paulino <contato@mateuspaulino.com.br>
2018-09-06 15:18:51 -0300

    creating funcion to remove classes

--- src/helpers/util.js
+++ src/helpers/util.js
@@ -1,5 +1,13 @@
-export default function func (param) {
+// export default function func (param) {
   
-    return param;
+//     return param;
     
+// }
+
+export default function removeClass (elements, className) {
+    //elements must be a HTML collection
+    elements[0].classList.remove(className);
+    if (elements[0]){
+        removeClass(elements, className);
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 // import Carousel from './components/Carousel';
+import removeClass from './helpers/util';
 import './../static/sass/main.scss';
 
 async function init() {
+
+  //bind querySelector
   const $ = document.querySelector.bind(document);
   const $$ = document.querySelectorAll.bind(document);
 
-  
+  //init variables
+  // let slideActual = 0;
 
-  // $('.carousel-container').innerHTML = Carousel();
+  //DOM elements
+  let slide = $$('.feedback__carousel__container__list__item');
+  let slideActivated = document.getElementsByClassName('feedback__carousel__container__list__item active');
+
+  const showSlide = (number) => {
+    removeClass(slideActivated,'active');
+    slide[number].classList.add('active');
+  }
+
+  //recursive function
+  // const removeClass = (elements, className) => {
+  //   //elements must be a HTML collection
+  //   elements[0].classList.remove(className);
+  //   if (elements[0]){
+  //     removeClass(elements, className);
+  //   }
+  // }
+
+  showSlide(1);
 }
 
 init();
